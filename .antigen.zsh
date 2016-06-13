1564c7216a7ca5f60aed758cce28ef136e5c9d7d
Christophe FEIJOO <cfeijoo@e2r13p12.42.fr>
2016-06-13 19:11:54 +0200

    First version

---
+++ .antigen.zsh
@@ -0,0 +1,9 @@
+source $HOME/.antigen/antigen.zsh
+
+# Antigen config
+antigen use oh-my-zsh
+antigen bundle git
+antigen bundle command-not-found
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen theme miloshadzic
+antigen apply
---
+++ install.sh
@@ -0,0 +1,13 @@
+# Create .zshrc that will source all dotfiles
+cat > ~/.zshrc <<EOL
+source ~/.dotfiles/*.zsh
+EOL
+
+# Install antigen
+curl -L https://raw.githubusercontent.com/zsh-users/antigen/master/antigen.zsh > $HOME/.antigen/antigen.zsh
+
+# Install 42 Homebrew Fix
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/kube/42homebrew_fix/master/install.sh)"
+
+# Apply config
+source ~/.zshrc
