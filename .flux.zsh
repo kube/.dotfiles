0b2cf96676aeed4eb6eab68234161e69dda91668
Christophe FEIJOO <cfeijoo@e2r13p12.42.fr>
2016-06-15 13:56:15 +0200

    Add f.lux

---
+++ .flux.zsh
@@ -0,0 +1,6 @@
+rm -rf ~/Library/Application\ Support/Flux
+rm -rf ~/Library/Containers/com.justgetflux.flux
+mkdir -p /tmp/Flux
+mkdir -p /tmp/com.justgetflux.flux
+ln -s /tmp/Flux ~/Library/Application\ Support/Flux
+ln -s /tmp/com.justgetflux.flux ~/Library/Containers/com.justgetflux.flux
