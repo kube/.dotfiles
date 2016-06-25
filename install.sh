bfbeee958757170c386c88805b51497800f6d37c
Christophe Feijoo <christophe@feijoo.fr>
2016-06-25 13:51:27 +0200

    Remove HomebrewFix install and protect .zshrc config

--- install.sh
+++ install.sh
@@ -1,22 +1,17 @@
 # Create .zshrc that will source all dotfiles
+if ! grep -q "# Source all dotfiles" "$HOME/.zshrc"; then
+
 cat > ~/.zshrc <<EOL
 # Source all dotfiles
 for f in ~/.dotfiles/.*.zsh; do source \$f; done
 EOL
 
+fi
+
 # Install antigen
 mkdir -p $HOME/.antigen
 curl -L https://raw.githubusercontent.com/zsh-users/antigen/master/antigen.zsh > $HOME/.antigen/antigen.zsh
 
-# Install 42 Homebrew Fix
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/kube/42homebrew_fix/master/install.sh)"
-
 # Apply config
 for f in ~/.dotfiles/.*.zsh; do source $f; done
 EOL
-
-# Install Node
-brew install node
-
-# Install Python
-brew install python
