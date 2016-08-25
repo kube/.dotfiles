be13b6ed6012923b97590dbf56c91ab1e1de73de
Christophe FEIJOO <cfeijoo@e2r13p10.42.fr>
2016-08-25 16:18:02 +0200

    Remove flux fix

--- .flux.zsh
+++
@@ -1,6 +0,0 @@
-rm -rf ~/Library/Application\ Support/Flux
-rm -rf ~/Library/Containers/com.justgetflux.flux
-mkdir -p /tmp/Flux
-mkdir -p /tmp/com.justgetflux.flux
-ln -s /tmp/Flux ~/Library/Application\ Support/Flux
-ln -s /tmp/com.justgetflux.flux ~/Library/Containers/com.justgetflux.flux
