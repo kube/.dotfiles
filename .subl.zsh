953bbe91ba2889e62c86cb3a1467860045a771ad
Chris Feijoo <christophe@feijoo.fr>
2016-10-11 11:27:09 +0200

    Fix Sublime Text helper PATH

--- .subl.zsh
+++ .subl.zsh
@@ -1,2 +1,2 @@
 # Sublime Text subl Helper
-export PATH="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/":$PATH
+export PATH="/Applications/Sublime Text.app/Contents/SharedSupport/bin/":$PATH
