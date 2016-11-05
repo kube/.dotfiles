0fbe20ae33dfb256f5f8f19f525b745fed02ab81
Chris Feijoo <christophe@feijoo.fr>
2016-11-05 20:38:00 +0100

    Clean

--- install.sh
+++ install.sh
@@ -13,6 +13,7 @@
 rm -rf $HOME/.zgen
 git clone https://github.com/tarjoilija/zgen $HOME/.zgen
 
+
 # Append dotfiles sourcing to .zshrc
 if ! grep -q "# Dotfiles sourcing" ~/.zshrc; then
 cat >> ~/.zshrc <<EOL
@@ -21,6 +22,7 @@ cat >> ~/.zshrc <<EOL
 source \$HOME/.dotfiles/index.sh
 EOL
 fi
+
 
 # Reset zsh to finish installation
 exec zsh -l
